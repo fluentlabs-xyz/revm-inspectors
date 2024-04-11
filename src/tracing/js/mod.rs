75071ccbe753f723e6132a424e39972b08764c19
Dmitry Savonin <nwtsoas@gmail.com>
2024-04-11 23:46:13 +0400

    fixed error trait

--- src/tracing/js/mod.rs
+++ src/tracing/js/mod.rs
@@ -369,7 +369,6 @@ impl JsInspector {
 impl<DB> Inspector<DB> for JsInspector
 where
     DB: IJournaledTrie,
-    <DB as DatabaseRef>::Error: std::fmt::Display,
 {
     fn step(&mut self, interp: &mut Interpreter, context: &mut EvmContext<DB>) {
         // if self.step_fn.is_none() {
